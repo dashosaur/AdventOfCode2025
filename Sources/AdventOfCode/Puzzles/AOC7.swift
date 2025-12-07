afa5e074be70121e737e261f9498f4cda30fd6fc
Dash Brittain <roxanne.brittain@gmail.com>
2025-12-06 21:20:51 -0800

    AOC7

--- Sources/AdventOfCode/Puzzles/AOC7.swift
+++ Sources/AdventOfCode/Puzzles/AOC7.swift
@@ -11,10 +11,50 @@ struct AOC7: Puzzle {
     typealias Answer = Int
     
     func solve1(input: String) -> Int {
-        input.lines.count
+        let grid = input.characterGrid
+        let start = grid.points.first(where: { grid[$0] == "S" })!
+        
+        var splitCount = 0
+        var beamLocations = Set<Point>([start])
+        while !beamLocations.isEmpty {
+            var nextLocations = Set<Point>()
+            for location in beamLocations {
+                let nextLocation = location + .init(0, 1)
+                if grid[nextLocation] == "^" {
+                    nextLocations.insert(nextLocation - .init(1, 0))
+                    nextLocations.insert(nextLocation + .init(1, 0))
+                    splitCount += 1
+                } else if grid[nextLocation] == "." {
+                    nextLocations.insert(nextLocation)
+                }
+            }
+            beamLocations = nextLocations
+        }
+        
+        return splitCount
     }
     
     func solve2(input: String) -> Int {
-        input.lines.count
+        let grid = input.characterGrid
+        let start = grid.points.first(where: { grid[$0] == "S" })!
+        
+        var timelineCountCache = [Point: Int]()
+        func countTimelinesFromBeamLocation(_ beamLocation: Point) -> Int {
+            if let cachedCount = timelineCountCache[beamLocation] { return cachedCount }
+            let nextLocation = beamLocation + .init(0, 1)
+            
+            let count: Int
+            if grid[nextLocation] == "^" {
+                count = countTimelinesFromBeamLocation(nextLocation - .init(1, 0)) + countTimelinesFromBeamLocation(nextLocation + .init(1, 0))
+            } else if grid[nextLocation] == "." {
+                count = countTimelinesFromBeamLocation(nextLocation)
+            } else {
+                count = 1
+            }
+            timelineCountCache[beamLocation] = count
+            return count
+        }
+        
+        return countTimelinesFromBeamLocation(start)
     }
 }
